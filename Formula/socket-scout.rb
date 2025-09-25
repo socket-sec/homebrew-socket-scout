9ba0c0d51dc62bccf1584b5eabf455e6e4ef0d22
m36 <matthewpitcher36@gmail.com>
2025-09-25 21:58:17 +0000

    Updated hash

--- Formula/socket-scout.rb
+++ Formula/socket-scout.rb
@@ -2,9 +2,9 @@ ruby
  class SocketScout < Formula
    desc "Installation of Socket Scout"
    homepage "https://github.com/socket-sec/socket-scout"
-   url "https://github.com/socket-sec/socket-scout.git"
-   version "1.0.0"
-   sha256 "your_sha256_hash_here"
+   url "https://github.com/socket-sec/socket-scout/archive/refs/tags/v1.1.9.tar.gz"
+   version "1.1.9"
+   sha256 "bcefcd46f9fa2702c67714984ec72b190ea37bcf12ddbcbf04a8d8691e43b9a8"
    license "MIT"
 
    def install
