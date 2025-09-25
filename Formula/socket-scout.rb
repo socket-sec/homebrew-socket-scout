db911e42933284e37d7c3d168bcc222da7f029cd
socket-sec <matthewpitcher36@gmail.com>
2025-09-25 16:10:26 -0600

    Fixed variable error

--- Formula/socket-scout.rb
+++ Formula/socket-scout.rb
@@ -1,13 +1,12 @@
-ruby
- class SocketScout < Formula
-   desc "Installation of Socket Scout"
-   homepage "https://github.com/socket-sec/socket-scout"
-   url "https://github.com/socket-sec/socket-scout/archive/refs/tags/v1.1.9.tar.gz"
-   version "1.1.9"
-   sha256 "bcefcd46f9fa2702c67714984ec72b190ea37bcf12ddbcbf04a8d8691e43b9a8"
-   license "MIT"
+class SocketScout < Formula
+  desc "Installation of Socket Scout"
+  homepage "https://github.com/socket-sec/socket-scout"
+  url "https://github.com/socket-sec/socket-scout/archive/refs/tags/v1.1.9.tar.gz"
+  version "1.1.9"
+  sha256 "bcefcd46f9fa2702c67714984ec72b190ea37bcf12ddbcbf04a8d8691e43b9a8"
+  license "MIT"
 
-   def install
-     bin.install "scripts/ssdev.py"
-   end
- end
+  def install
+    bin.install "scripts/ssdev.py"
+  end
+end
