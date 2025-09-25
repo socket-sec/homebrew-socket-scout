b3a01e8aa310da5009f2edea0655f57d43ae566e
m36 <matthewpitcher36@gmail.com>
2025-09-25 23:03:19 +0000

    Updated version

--- Formula/socket-scout.rb
+++ Formula/socket-scout.rb
@@ -2,7 +2,7 @@ class SocketScout < Formula
   desc "Installation of Socket Scout"
   homepage "https://github.com/homebrew-socket-sec/socket-scout"
   url "https://github.com/socket-sec/socket-scout/archive/refs/tags/v2.0.0.tar.gz"
-  version "2.0.2"
+  version "2.0.3"
   sha256 "cad0649ebd01ab460002cec7293a776734c03ca7ac33deda55f4ab6b7ce4c700"
   license "MIT"
 
