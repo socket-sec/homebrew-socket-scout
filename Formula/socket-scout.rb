a55dffb51db774d8f7e28bdca135c33e1f8bb6f1
m36 <matthewpitcher36@gmail.com>
2025-09-25 22:25:34 +0000

    Updated missing variables for ssagent

--- Formula/socket-scout.rb
+++ Formula/socket-scout.rb
@@ -1,9 +1,9 @@
 class SocketScout < Formula
   desc "Installation of Socket Scout"
   homepage "https://github.com/homebrew-socket-sec/socket-scout"
-  url "https://github.com/socket-sec/socket-scout/archive/refs/tags/v1.1.9.tar.gz"
-  version "1.1.9"
-  sha256 "bcefcd46f9fa2702c67714984ec72b190ea37bcf12ddbcbf04a8d8691e43b9a8"
+  url "https://github.com/socket-sec/socket-scout/archive/refs/tags/v2.0.0.tar.gz"
+  version "2.0.0"
+  sha256 "be71ae77f99925b70933a374d495458133db7d40024ad7de9557059a53d701b5"
   license "MIT"
 
   def install
