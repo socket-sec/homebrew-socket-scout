54a88eb20ac9c851b0a7c77586d67a2f417eb1c4
socket-sec <matthewpitcher36@gmail.com>
2025-09-25 16:11:26 -0600

    Variable Update

--- Formula/socket-scout.rb
+++ Formula/socket-scout.rb
@@ -1,6 +1,6 @@
 class SocketScout < Formula
   desc "Installation of Socket Scout"
-  homepage "https://github.com/socket-sec/socket-scout"
+  homepage "https://github.com/homebrew-socket-sec/socket-scout"
   url "https://github.com/socket-sec/socket-scout/archive/refs/tags/v1.1.9.tar.gz"
   version "1.1.9"
   sha256 "bcefcd46f9fa2702c67714984ec72b190ea37bcf12ddbcbf04a8d8691e43b9a8"
