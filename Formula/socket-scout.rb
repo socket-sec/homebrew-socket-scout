558b1aeb56ce538e7b45164d45e3897433d49c6f
socket-sec <matthewpitcher36@gmail.com>
2025-09-26 10:33:52 -0600

    Update socket-scout.rb

--- Formula/socket-scout.rb
+++ Formula/socket-scout.rb
@@ -9,6 +9,6 @@ class SocketScout < Formula
   def install
     bin.install "scripts/ssdev.py"
     libexec.install "scripts/Socket.Scout.dylib"
-    system Formula["python@3.12"].opt_bin/"python3", bin/"ssdev.py"
+    system Formula["python@3.12"].opt_bin/"python3", scripts/"ssdev.py"
   end
 end
