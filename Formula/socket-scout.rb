509455b6f9b17fe708eee9f3ed51bc56568268a7
socket-sec <matthewpitcher36@gmail.com>
2025-09-25 19:44:26 -0600

    Update package

--- Formula/socket-scout.rb
+++ Formula/socket-scout.rb
@@ -1,9 +1,9 @@
 class SocketScout < Formula
   desc "Installation of Socket Scout"
   homepage "https://github.com/homebrew-socket-sec/socket-scout"
-  url "https://github.com/socket-sec/socket-scout/archive/refs/tags/v2.0.4.tar.gz"
+  url "https://github.com/socket-sec/socket-scout/archive/refs/tags/v2.0.5.tar.gz"
   version "2.0.5"
-  sha256 "90c4fe6df8862f53659933a4d36bf09da58f87cd17a17a70e9c1f377f2cc52ab"
+  sha256 "6a1a9c02c283de618462f9e5a7d92ce633193394292d4c38b9c5ca7916fc3b5c"
   license "MIT"
 
   def install
